8ca2e1300f149f06c732890dc2a85bcb545ffc8b
Jonas Primbs <jonas.primbs@uni-tuebingen.de>
2023-08-14 15:22:56 +0200

    bug fixes

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -5,7 +5,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { IdentityMenuComponent } from './components/identity-menu/identity-menu.component';
 import { ProfileImageComponent } from './components/profile-image/profile-image.component';
@@ -20,7 +19,6 @@ import { OidcRedirectComponent } from './pages/oidc-redirect/oidc-redirect.compo
     MatIconModule,
     MatMenuModule,
     MatTooltipModule,
-    OAuthModule.forRoot(),
   ],
   declarations: [
     IdentityMenuComponent,

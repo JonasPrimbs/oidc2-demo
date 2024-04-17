42a42383d4ee56823f1b217093c04f1dcb807a6d
Jonas Primbs <jonas.primbs@uni-tuebingen.de>
2024-04-17 10:58:05 +0200

    cleanup

--- src/app/modules/authentication/classes/authentication-options/authentication-options.class.spec.ts
+++ src/app/modules/authentication/classes/authentication-options/authentication-options.class.spec.ts
@@ -2,6 +2,6 @@ import { AuthenticationOptions } from './authentication-options.class';
 
 describe('AuthenticationOptions', () => {
   it('should create an instance', () => {
-    expect(new AuthenticationOptions()).toBeTruthy();
+    expect(new AuthenticationOptions([])).toBeTruthy();
   });
 });

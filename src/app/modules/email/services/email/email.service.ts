152b4d4683363b3743f54192f372eb2fff051982
Joel Schmid <joelschmidwitt@gmail.com>
2024-10-17 12:30:42 +0200

    refactoring email

--- src/app/modules/email/services/email/email.service.ts
+++ src/app/modules/email/services/email/email.service.ts
@@ -1,6 +1,5 @@
 import * as openpgp from 'openpgp';
 
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Identity, IdentityService } from '../../../authentication';
@@ -9,9 +8,8 @@ import { decodeAndParseMimeMessage, MimeMessage } from '../../classes/mime-messa
 import { GmailApiService } from '../gmail-api/gmail-api.service';
 import { PgpService } from '../pgp/pgp.service';
 import { AttachmentFile } from '../../classes/attachment-file/attachment-file';
-import { E2ePopPgpOptions } from '../../types/e2e-pop-pgp-options.interface';
-
-import * as jose from 'jose';
+import { encodeBase64url } from 'src/app/byte-array-converter/base64url';
+import { EmailPart } from '../../types/email-part.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -69,11 +67,11 @@ export class EmailService {
     if(encrypted){
       let encryptionKeys = this.pgpService.getEncryptionKeys(email.sender, email.receiver);
       if(encryptionKeys!== undefined){
-        emailString = await email.toRawEncryptedMimeString(encryptionKeys, privateKey, passphrase);
+        emailString = await this.getEmailRawEncryptedMimeString(email, encryptionKeys, privateKey, passphrase);
       }
     }
     else{
-      emailString = await email.toRawMimeString(privateKey, passphrase);
+      emailString = await this.getEmailRawMimeString(email, privateKey, passphrase);
     }
     if(emailString !== undefined){
       let res = await this.gmailApiService.sendMail(email.sender, emailString);
@@ -81,4 +79,208 @@ export class EmailService {
     }
     return false;
   }  
+
+  /**
+   * Encrypts the email string with PGP and returns as raw string (base64url encoded)
+   * @param encryptionKeys PGP Public Keys of the receivers to encrypt the email with.
+   * @param pgpPrivateKey PGP Private Key of the sender to sign the email with.
+   * @param passphrase Passphrase of the PGP Private Key to use it.
+   * @returns PGP-encrypted email.
+   */
+   public async getEmailRawEncryptedMimeString(email: Email, encryptionKeys: openpgp.PublicKey[], pgpPrivateKey: openpgp.PrivateKey, passphrase: string): Promise<string> {
+    const encryptedMailString = await this.getEncryptedMimeString(email, encryptionKeys, pgpPrivateKey, passphrase);
+    return window.btoa(encryptedMailString)
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/\=/g, '');  
+   }
+
+  /**
+   * Encodes the email Base64URl.
+   * @returns Base64URL encoded email.
+   */
+  public async getEmailRawMimeString(email: Email, pgpPrivateKey?: openpgp.PrivateKey, passphrase?: string): Promise<string> {
+    // Get email string.
+    const mailString = await this.getMimeString(email, pgpPrivateKey, passphrase);
+    return window.btoa(mailString)
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/\=/g, '');    
+  }
+
+  /**
+   * Signs an email and returns the signed email string.
+   * @param pgpPrivateKey PGP Private key for signing.
+   * @param passphrase Passphrase for the PGP Private Key.
+   * @returns Signed email string.
+   */
+   public async getMimeString(email: Email, pgpPrivateKey?: openpgp.PrivateKey, passphrase?: string): Promise<string> {
+
+    let privateKey: openpgp.PrivateKey | undefined = undefined;
+
+    if(pgpPrivateKey && passphrase && !pgpPrivateKey.isDecrypted()){
+      // decrypts the private key
+      privateKey = await openpgp.decryptKey({
+        privateKey: pgpPrivateKey,
+        passphrase: passphrase,
+      });
+    }
+
+    if(pgpPrivateKey?.isDecrypted()){
+      privateKey = pgpPrivateKey;
+    }
+
+    let publicKey: openpgp.PublicKey | undefined = undefined;
+    let publicKeyAttachment: AttachmentFile | undefined;
+
+    if(privateKey){
+      // Get public key.
+      publicKey = privateKey.toPublic();
+      // Get public key ID.
+      const publicKeyId = `0x${publicKey.getKeyID().toHex()}`;
+      // Get public key as armored string.
+      const publicKeyString = publicKey.armor();
+  
+      // Get Public Key as attachment file.
+      publicKeyAttachment = new AttachmentFile(
+        `OpenPGP_${publicKeyId.toUpperCase()}.asc`,
+        publicKeyString,
+        'application/pgp-keys',
+        'OpenPGP public key',
+      );
+    }
+
+    // skip public key attachment if not available
+    let bodyParts = [...email.parts, publicKeyAttachment].filter(p => p !== undefined) as EmailPart[];    
+
+    // Prepare signed content.
+    const innerBoundary = this.generateRandomBoundry();
+    const innerArr = [
+      // MIME Multipart header:
+      this.headerToString(email.getMultipartHeader(innerBoundary)),
+
+      // Body parts:
+      ...bodyParts.map(part => [
+        // MIME Header:
+        `--${innerBoundary}\r\n${this.headerToString(part.getMimeHeader())}`,
+        // Body:
+        part.getBody(),
+      ]).reduce((prev, curr) => [...prev, ...curr]),
+
+      // End:
+      `--${innerBoundary}--\r\n`,
+    ];
+    const body = innerArr.join('\r\n\r\n');
+
+    // sign the message, if the private key is available
+    let signatureFileAttachment: AttachmentFile | undefined;
+    if(privateKey){
+      // Create signature.
+      const pgpMessage = await openpgp.createMessage({ text: body });
+      const signatrueString = await openpgp.sign({
+        message: pgpMessage,
+        signingKeys: privateKey,
+        detached: true,
+        format: 'armored',
+      });
+          
+      signatureFileAttachment = new AttachmentFile(
+        'OpenPGP_signature.asc',
+        signatrueString,
+        'application/pgp-signature',
+        'OpenPGP digital signature',
+      );
+    }
+
+    const outerBoundary = this.generateRandomBoundry();
+
+    let signaturePart = '';
+    if(signatureFileAttachment){
+      signaturePart = `\r\n--${outerBoundary}\r\n${this.headerToString(signatureFileAttachment.getMimeHeader())}\r\n\r\n${signatureFileAttachment.getBody()}`;
+
+    }
+
+    const outerArr = [
+      // MIME Multipart header:
+      this.headerToString(email.getMultipartSignedHeader(outerBoundary)),
+
+      // Signed message + signature if available:
+      `--${outerBoundary}\r\n${body}${signaturePart}`,
+
+      // End:
+      `--${outerBoundary}--`,
+    ];
+    return outerArr.join('\r\n\r\n');
+  }
+
+  /**
+   * Encrypts the email string with PGP.
+   * @param encryptionKeys PGP Public Keys of the receivers to encrypt the email with.
+   * @param pgpPrivateKey PGP Private Key of the sender to sign the email with.
+   * @param passphrase Passphrase of the PGP Private Key to use it.
+   * @returns PGP-encrypted email.
+   */
+  public async getEncryptedMimeString(email: Email, encryptionKeys: openpgp.PublicKey[], pgpPrivateKey: openpgp.PrivateKey, passphrase: string): Promise<string> {
+    // Get signed and unencrypted email string.
+    const emailString = await this.getMimeString(email, pgpPrivateKey, passphrase);
+
+    // Create PGP message from emailString.
+    const pgpMessage = await openpgp.createMessage({ text: emailString });
+
+    // Encrypt the PGP message with the provided public key.
+    const encryptedMailContent = await openpgp.encrypt({
+      message: pgpMessage,
+      encryptionKeys,
+    });
+
+    const outerBoundary = this.generateRandomBoundry();
+
+    // attachment with encrypted content
+    const encryptedAttachment = new AttachmentFile("encrypted.asc", encryptedMailContent, "application/octet-stream", "OpenPGP encrypted message");   
+    
+    // this part is equal for all pgp encrypted mails:
+    const pgpMimeVersionPart = `Content-Type: application/pgp-encrypted
+    Content-Description: PGP/MIME version identification
+
+    Version: 1`;    
+    
+    const outerArr = [
+      // MIME Multipart header:
+      this.headerToString(email.getMultipartEncryptedHeader(outerBoundary)),
+
+      // pgp Mime Version:
+      `--${outerBoundary}\r\n${pgpMimeVersionPart}\r\n--${outerBoundary}`,
+      
+      // encrypted content:
+      this.headerToString(encryptedAttachment.getMimeHeader()),      
+      encryptedAttachment.getBody(),
+
+      // End:
+      `--${outerBoundary}--`,
+    ];
+
+    let finalEncryptedMail = outerArr.join('\r\n\r\n');
+    
+    return finalEncryptedMail;  
+  }
+
+  /**
+   * Stringifies a MIME header.
+   * @param header Header instance.
+   * @returns String-encoded header.
+   */
+   private headerToString(header: Record<string, string>): string {
+    return Object.keys(header).map(key => `${key}: ${header[key]}`).join('\r\n');
+  }
+
+  /**
+   * Generates a random Boundry string.
+   * @param byteLength Length of the string in bytes.
+   * @returns Random String.
+   */
+  private generateRandomBoundry(byteLength: number = 16): string {
+    const arrayBuffer = new Uint8Array(byteLength);
+    const randomBytes = crypto.getRandomValues(arrayBuffer);
+    return `---------${encodeBase64url(randomBytes)}`;
+  }
 }
--- src/app/modules/email/services/gmail-api/gmail-api.service.ts
+++ src/app/modules/email/services/gmail-api/gmail-api.service.ts
@@ -2,33 +2,15 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { firstValueFrom } from "rxjs";
 import { Identity } from "src/app/modules/authentication";
-import { AttachmentFile } from "../../classes/attachment-file/attachment-file";
-import { Email } from "../../classes/email/email";
-import { decodeAndParseMimeMessage, MimeMessage } from "../../classes/mime-message/mime-message";
-import { PublicKeyOwnership, PublicKeyOwnershipExtended } from "../../types/public-key-ownership.interface";
-import { TrustworthyIctIssuer, TrustworthyIctIssuerExtended } from "../../types/trustworthy-ict-issuer";
-
-import * as openpgp from 'openpgp';
-import { OnlinePrivateKey as OnlinePrivateKey } from "../../types/online-private-key.interface";
-import { PgpKeyCertificationService } from "../pgp-key-certification/pgp-key-certification.service";
-import { EmailContent } from "../../classes/email-content/email-content";
 
 @Injectable({
   providedIn: 'root',
 })
 export class GmailApiService {
 
-  readonly privateKeyLabelName = "PRIVATE_KEY";
-  readonly publicKeyLabelName = "PUBLIC_KEY";
-  readonly trustworthyIctIssuerLabelName = "TRUSTWORTHY_ICT_ISSUER";
-
-  public readonly publicKeyAttachmentFileName = "public_key.asc";
-  public readonly privateKeyAttachmentFileName = "private_key.asc";
-  public readonly trustworthyIctIssuerAttachmentFileName = "trustworthy_ict_issuer.txt";
-
   constructor(
     private readonly http: HttpClient,
-    private readonly oidc2AttachmentService: PgpKeyCertificationService,
+    
   ){}
 
   /**
@@ -204,179 +186,6 @@ export class GmailApiService {
         },
       },
     ));
-  }
-
-  /**
-   * saves attachment-file data to a mail with a specific label
-   * @param identity 
-   * @param subject 
-   * @param attachments 
-   * @param labelName 
-   * @returns 
-   */
-  public async saveData(identity: Identity, subject: string, attachments: AttachmentFile[], labelName: string, ictIdentity: Identity, privateKey: openpgp.PrivateKey, passphrase: string, explanation: string, encrypted: boolean = false): Promise<MessageResult | undefined>{    
-    let emailBody = new EmailContent(explanation);
-    const email = new Email(identity, identity.claims.email ?? "", subject, [emailBody, ...attachments]);
-
-    const ictPopAttachments = await this.oidc2AttachmentService.generateIctPopAttachments([ictIdentity], email.receiver, privateKey);
-
-    ictPopAttachments.forEach(a => email.parts.push(a));
-
-    let mimeMessage: string = "";
-    if(encrypted){
-      let encryptionKey = await openpgp.decryptKey({privateKey, passphrase});      
-      mimeMessage = await email.toRawEncryptedMimeString([encryptionKey], privateKey, passphrase)
-    }
-    else{
-      mimeMessage = await email.toRawMimeString(privateKey, passphrase);
-    }
-    console.log(mimeMessage);
-
-    let message = await this.importMail(identity, mimeMessage);
-    if(!message){
-      return undefined;
-    }
-
-    if(message && !labelName){
-      return message;
-    }
-
-    let labels = await this.getLabels(identity);
-
-    let label = labels?.find(l => l.name === labelName);
-
-    if(label === undefined){
-      label = await this.createLabel(identity, labelName);
-    }   
-
-    if(label !== undefined){
-      await this.modifyMailLabels(identity, message.id, [ label ]);
-    }
-
-    return message;
-  }
-
-
-  /**
-   * save the private key of a gmail identity
-   * @param identity 
-   * @param attachment 
-   */
-  public async savePrivateKey(identity: Identity, ictIdentity: Identity, privateKey: openpgp.PrivateKey, passphrase: string): Promise<string|undefined> { 
-    let explanation = `The attachment ${this.privateKeyAttachmentFileName} contains your encrypted private key with the KeyId 0x${ privateKey.getKeyID().toHex().toUpperCase() }.
-    After deleting this mail you can neither decrypt nor sign mails with this private key.`;
-    const attachment = new AttachmentFile(this.privateKeyAttachmentFileName, privateKey.armor(), "text/plain");    
-    let message = await this.saveData(identity, "private_key", [attachment], this.privateKeyLabelName, ictIdentity, privateKey, passphrase, explanation);
-    return message?.id;
-  }
-
-  /**
-   * load all private keys of a gmail identity
-   * @param identity 
-   * @returns 
-   */
-  public async loadPrivateKeys(identity: Identity): Promise<OnlinePrivateKey[]>{
-    let listMailResult = await this.listMails(identity, `label:${this.privateKeyLabelName}`);
-    let privateKeys : OnlinePrivateKey[] = [];
-    if(!listMailResult){
-      return [];
-    }
-    for (let mail of listMailResult.messages){
-      let message = await this.getMessage(identity, mail.id);
-      if(message?.raw){
-        let parsedMimeMessage = decodeAndParseMimeMessage(message.raw);
-        let privateKeyAttachment = parsedMimeMessage.payload.attachments.filter(a => a.name === this.privateKeyAttachmentFileName);
-        for(let attachment of privateKeyAttachment){
-          let privateKey = await openpgp.readPrivateKey({ armoredKey: attachment.decodedText() })
-          privateKeys.push({identity, messageId: mail.id, privateKey, mimeMessage: parsedMimeMessage});
-        }
-      }
-    }
-    return privateKeys;
-  }
-
-  /**
-   * save the public key into gmail
-   * @param identity 
-   * @param attachment 
-   * @param sender 
-   * @returns 
-   */
-  public async savePublicKey(identity: Identity, publicKey: openpgp.PublicKey, sender: string, ictIdentity: Identity, privateKey: openpgp.PrivateKey, passphrase: string) : Promise<string | undefined>{
-    let explanation = `The attachment ${this.publicKeyAttachmentFileName} contains the PGP-public-key of ${sender}. 
-    After deleting this mail you can't encrypt a mail for ${sender} with the Key 0x${publicKey.getKeyID().toHex().toUpperCase()}.`;
-    const attachment = new AttachmentFile(this.publicKeyAttachmentFileName, publicKey.armor(), "text/plain");
-    let message = await this.saveData(identity, sender, [attachment], this.publicKeyLabelName, ictIdentity, privateKey, passphrase, explanation, true);
-    console.log(message);
-    return message?.id;
-  }
-
-  /**
-   * Load the trusted public keys of a gmail identity
-   * @param identity 
-   * @returns 
-   */
-   public async loadPublicKeyOwnerships(identity: Identity) : Promise<PublicKeyOwnershipExtended[]>{
-    let listMailsResult = await this.listMails(identity, `label:${this.publicKeyLabelName}`);
-    let publicKeyOwnerships: PublicKeyOwnershipExtended[] = [];
-    if(!listMailsResult){
-      return [];
-    }
-    for(let mail of listMailsResult.messages){
-      let message = await this.getMessage(identity, mail.id);
-      if(message?.raw){
-        let parsedMimeMessage = decodeAndParseMimeMessage(message.raw);
-        publicKeyOwnerships.push({ 
-          identity: identity, 
-          messageId: mail.id,
-          mimeMessage: parsedMimeMessage,
-        });
-      }
-    }
-    return publicKeyOwnerships;
-  }
-
-  /**
-   * save a trustworthy ict issuer to gmail
-   * @param identity 
-   * @param issuer 
-   * @returns 
-   */
-  public async saveTrustworthyIctIssuer(identity: Identity, issuer: string, ictIdentity: Identity, privateKey: openpgp.PrivateKey, passphrase: string) : Promise<string | undefined> {
-    let explanation = `the attachment ${this.trustworthyIctIssuerAttachmentFileName} contains your trust into the ICT-issuer ${issuer}.
-    After deleting this mail you revoke your trust into the ICT issuer.`;
-    let attachment = new AttachmentFile(this.trustworthyIctIssuerAttachmentFileName, issuer, "text/plain", "trustworthy_ict_issuer");
-    let message = await this.saveData(identity, "ict_issuer", [ attachment ], this.trustworthyIctIssuerLabelName, ictIdentity, privateKey, passphrase, explanation);
-    return message?.id;
-  }
-
-  /**
-   * Load the trustworty ict issuer of a identity
-   * @param identity 
-   * @returns 
-   */
-  public async loadTrustworthyIctIssuers(identity: Identity) : Promise<TrustworthyIctIssuerExtended[]>{
-    let listMailsResult = await this.listMails(identity, `label:${this.trustworthyIctIssuerLabelName}`);
-    let trustworthyIctIssuers: TrustworthyIctIssuerExtended[] = [];
-    if(!listMailsResult){
-      return [];
-    }
-    for(let mail of listMailsResult.messages){
-      let message = await this.getMessage(identity, mail.id);
-      if(message?.raw){
-        let parsedMimeMessage = decodeAndParseMimeMessage(message.raw);
-        let trustworthyIssuerAttachments = parsedMimeMessage.payload.attachments.filter(a => a.name === this.trustworthyIctIssuerAttachmentFileName);
-        for(let attachment of trustworthyIssuerAttachments){
-          trustworthyIctIssuers.push({ 
-            identity: identity, 
-            issuer: attachment.decodedText().trim(), 
-            messageId: mail.id,
-            mimeMessage: parsedMimeMessage,
-          });
-        }
-      }
-    }
-    return trustworthyIctIssuers;
   }
 }
 
